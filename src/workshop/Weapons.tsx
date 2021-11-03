9b19ac5ebfa3edefaf262c843a2ae645acfebd8c
Nicho Hoffman <nick@corebridge.net>
2021-11-03 10:48:07 -0600

    Customize BossDragon for Victory

--- src/workshop/Weapons.tsx
+++ src/workshop/Weapons.tsx
@@ -30,27 +30,87 @@ export class Club implements IMeleeWeapon {
   meleeRange = 1;
 }
 
+export class FireMagic implements IEnchantedItem {
+  name = "Fire Magic";
+  fireDamage = 3;
+}
+
+export class Firebolt extends FireMagic implements IWeapon {
+  name = "Firebolt";
+  damage = 3;
+}
+
+export class MagicWand implements IRangedWeapon<Firebolt> {
+  name = 'Magic Wand';
+  damage: 0 = 0;
+  projectiles = [
+    new Firebolt, new Firebolt, new Firebolt, new Firebolt, new Firebolt,
+    new Firebolt, new Firebolt, new Firebolt, new Firebolt, new Firebolt,
+    new Firebolt, new Firebolt, new Firebolt, new Firebolt, new Firebolt,
+    new Firebolt, new Firebolt, new Firebolt, new Firebolt, new Firebolt,
+    new Firebolt, new Firebolt, new Firebolt, new Firebolt, new Firebolt,
+  ];
+}
+
+export class Arrow implements IWeapon {
+  name = "Arrow";
+  damage = 3;
+}
+
+export class LongBow implements IRangedWeapon<Arrow> {
+  name = "Long Bow";
+  damage: 0 = 0;
+  projectiles = [
+    new Arrow, new Arrow, new Arrow, new Arrow, new Arrow,
+    new Arrow, new Arrow, new Arrow, new Arrow, new Arrow,
+    new Arrow, new Arrow, new Arrow, new Arrow, new Arrow,
+    new Arrow, new Arrow, new Arrow, new Arrow, new Arrow,
+    new Arrow, new Arrow, new Arrow, new Arrow, new Arrow,
+  ];
+}
+
+
+export class FlamingSword extends FireMagic implements IMeleeWeapon {
+  name = "Flaming Sword";
+  damage = 3;
+  meleeRange = 1;
+  fireDamage = 3;
+}
+
+export class ArcaneDagger extends FireMagic implements IMeleeWeapon {
+  name = "Arcane Dagger";
+  damage = 3;
+  meleeRange = 2; // throwing and returning
+  fireDamage = 3;
+}
+
+
 // ITEM VAULT
-// todo: add more and better items!
-export class UselessAmulet implements IItem {
-  name = 'Useless Amulet';
+export class AmuletOfHealth implements IEnchantedItem {
+  name = 'Amulet Of Health';
+  partyHealthBonus = 5;
+}
+
+export class Potion implements IConsumableItem {
+  name = 'Potion';
+  healthBonus = 5;
 }
 
 
 // ITEM ASSIGNMENTS
 // todo: assign starting items
-export const WarriorStartItem: IItem|undefined = new Club();
-export const ClericStartItem: IItem|undefined = new Club();
-export const MageStartItem: IItem|undefined = undefined;
-export const ThiefStartItem: IItem|undefined = new Club();
+export const WarriorStartItem: IItem|undefined = new FlamingSword();
+export const ClericStartItem: IItem|undefined = new ArcaneDagger();
+export const MageStartItem: IItem|undefined = new MagicWand();
+export const ThiefStartItem: IItem|undefined = new ArcaneDagger();
 
 // TREASURE ASSIGNMENTS
 // todo: assign treasure from chests
 export function GetItemsInTreasureChests(): IItem[]{
   return [
-    new UselessAmulet(), //this will be found by the warrior
-    new UselessAmulet(), //this will be found by the cleric
-    new UselessAmulet(), //this will be found by the mage
-    new UselessAmulet(), //this will be found by the thief
+    new Potion(), //this will be found by the warrior
+    new AmuletOfHealth(), //this will be found by the cleric
+    new Potion(), //this will be found by the mage
+    new LongBow(), //this will be found by the thief
   ];
 }
