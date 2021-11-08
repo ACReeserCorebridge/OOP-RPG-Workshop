da2831ff6b83ad51d38ef49e2a079570133706ce
metsz <ebucog@corebridge.net>
2021-11-09 02:55:51 +0800

    How to kill a dragon! - Metsz

--- src/workshop/Characters.tsx
+++ src/workshop/Characters.tsx
@@ -1,123 +1,107 @@
 import { ICharacter, CharacterClassName, equip, ICharacterActionDecision } from '../off-limits/ICharacter';
-import { IWeapon, IItem, isMeleeWeapon } from '../off-limits/IWeapons';
+import { IWeapon, IItem, isMeleeWeapon, isRangedWeapon } from '../off-limits/IWeapons';
 import { Character } from './BaseCharacter';
 import {
+  ClericAmuletItem,
   ClericStartItem,
+  MageRingItem,
   MageStartItem,
+  ThiefDaggerItem,
   ThiefStartItem,
   WarriorStartItem,
 } from './Weapons';
 
-//todo: too many duplicate classes in this file! 
 //todo: customize the chooseAction() to better fight the dragon
-//todo: update the `getASCIIStatus` function(s) to return X when dead and a unique character per class
 
-export class Warrior implements ICharacter {
-  health: number = 5;
-  position: number = 10;
-  weapons: IWeapon[] = [];
-  item?: IItem;
-  feet = new Feet(this);
+export class Warrior extends Character {
   classname(): CharacterClassName {
     return 'Warrior';
   }
-  move(){
-    this.feet.move();
-  }
   constructor(public name: string, public key: number) {
+    super();
     equip(WarriorStartItem, this);
   }
-  chooseAction(): ICharacterActionDecision {
-    return {
-      attack: this.weapons[0]
+  chooseAction(): ICharacterActionDecision{
+    if (this.item && this.health <= 2) {
+      return {
+        use: this.item
+      }
     }
-  }
-  getASCIIStatus(): string {
-      return "@";
+
+    return super.chooseAction();
   }
 }
 
-export class Cleric implements ICharacter{
-  health: number = 5;
-  position: number = 10;
-  weapons: IWeapon[] = [];
-  item?: IItem;
-  feet = new Feet(this);
+export class Cleric extends Character {
   classname(): CharacterClassName {
     return 'Cleric';
   }
-  move(){
-    this.feet.move();
-  }
   constructor(public name: string, public key: number) {
+    super();
     equip(ClericStartItem, this);
   }
-  chooseAction(): ICharacterActionDecision {
-    return {
-      attack: this.weapons[0]
+  chooseAction(): ICharacterActionDecision{
+    if (this.health <= 3) {
+      if (!this.item) {
+        equip(ClericAmuletItem, this);
+      }
+
+      if (this.item) {
+        return {
+          use: this.item
+        }
+      }
     }
-  }
-  getASCIIStatus(): string {
-      return "@";
+  
+    return super.chooseAction();
   }
 }
 
-export class Mage implements ICharacter {
-  health: number = 5;
-  position: number = 10;
-  weapons: IWeapon[] = [];
-  item?: IItem;
-  feet = new Feet(this);
+export class Mage extends Character {
   classname(): CharacterClassName {
     return 'Mage';
   }
   constructor(public name: string, public key: number) {
+    super();
     equip(MageStartItem, this);
   }
-  move(){
-    this.feet.move();
-  }
-  chooseAction(): ICharacterActionDecision {
-    return {
-      attack: this.weapons[0]
+  chooseAction(): ICharacterActionDecision{
+    if (isRangedWeapon(this.weapons[0]) && this.weapons[0].projectiles.length === 0) {
+      if (!this.item) {
+        equip(MageRingItem, this);
+      }
+
+      if (this.item) {
+        return {
+          use: this.item
+        }
+      }
     }
-  }
-  getASCIIStatus(): string {
-      return "@";
+  
+    return super.chooseAction();
   }
 }
 
-export class Thief implements ICharacter {
-  health: number = 5;
-  position: number = 10;
-  weapons: IWeapon[] = [];
-  item?: IItem;
-  feet = new Feet(this);
-  move(){
-    this.feet.move();
-  }
+export class Thief extends Character {
   classname(): CharacterClassName {
     return 'Thief';
   }
   constructor(public name: string, public key: number) {
+    super();
     equip(ThiefStartItem, this);
   }
-  chooseAction(): ICharacterActionDecision {
-    return {
-      attack: this.weapons[0]
-    }
-  }
-  getASCIIStatus(): string {
-      return "@";
-  }
-}
 
-//todo: something about this class is code smell...
-export class Feet{
-  constructor(private character: ICharacter){}
-  move(){
-    if (this.character.weapons.some(x => isMeleeWeapon(x))){
-      this.character.position = Math.max(this.character.position - 5, 1);
+  chooseAction(): ICharacterActionDecision{
+    if (isRangedWeapon(this.weapons[0]) && this.weapons[0].projectiles.length === 0) {
+      if (!this.weapons.some(weap => weap.name === ThiefDaggerItem?.name)) {
+        equip(ThiefDaggerItem, this);
+      }
+
+      return {
+        attack: this.weapons.find(weap => weap.name === ThiefDaggerItem?.name) || this.weapons[0]
+      }
     }
+  
+    return super.chooseAction();
   }
 }
--- src/workshop/Weapons.tsx
+++ src/workshop/Weapons.tsx
@@ -23,34 +23,103 @@ import { IWeapon, IItem, IMeleeWeapon, IRangedWeapon, IConsumableItem, IEnchante
 // }
 
 // WEAPON ARMORY
-// todo: add more and better weapons!
 export class Club implements IMeleeWeapon {
   name = 'Club';
   damage = 1;
   meleeRange = 1;
 }
 
+export class Sword implements IMeleeWeapon {
+  name = 'Sword';
+  damage = 5;
+  meleeRange = 1;
+}
+
+export class Dagger implements IMeleeWeapon {
+  name = 'Dagger';
+  damage = 4;
+  meleeRange = 1;
+}
+
+export class Hammer implements IMeleeWeapon {
+  name = 'Hammer';
+  damage = 3;
+  meleeRange = 1;
+}
+
+export class Arrow implements IMeleeWeapon {
+  name = "Arrow";
+  damage = 1;
+  meleeRange = 1;
+}
+
+export class MagicArrow implements IMeleeWeapon {
+  name = "Magic Arrow";
+  damage = 2;
+  meleeRange = 1;
+}
+
+export class Bow implements IRangedWeapon<Arrow> {
+  name = "Bow";
+  damage: 0 = 0;
+  projectiles: Arrow[] = [
+    new Arrow(),
+    new Arrow(),
+    new Arrow()
+  ];
+}
+
+export class Staff implements IRangedWeapon<MagicArrow> {
+  name = "Staff";
+  damage: 0 = 0;
+  projectiles: MagicArrow[] = [
+    new MagicArrow(),
+    new MagicArrow(),
+    new MagicArrow()
+  ];
+}
+
+export class DivineAmulet implements IEnchantedItem {
+  name = "Divine Amulet";
+  partyHealthBonus = 2;
+}
+
+export class RingOfFire implements IEnchantedItem {
+  name = "Ring of Fire";
+  fireDamage = 3;
+}
+
 // ITEM VAULT
-// todo: add more and better items!
 export class UselessAmulet implements IItem {
   name = 'Useless Amulet';
 }
 
+export class SmallHealthPotion implements IConsumableItem {
+  name = 'Small Health Potion';
+  healthBonus = 2;
+}
+
+export class LargeHealthPotion implements IConsumableItem {
+  name = 'Large Health Potion';
+  healthBonus = 3;
+}
+
 
 // ITEM ASSIGNMENTS
-// todo: assign starting items
-export const WarriorStartItem: IItem|undefined = new Club();
-export const ClericStartItem: IItem|undefined = new Club();
-export const MageStartItem: IItem|undefined = undefined;
-export const ThiefStartItem: IItem|undefined = new Club();
+export const WarriorStartItem: IItem|undefined = new Sword();
+export const ClericStartItem: IItem|undefined = new Hammer();
+export const MageStartItem: IItem|undefined = new Staff();
+export const ThiefStartItem: IItem|undefined = new Bow();
+export const ClericAmuletItem: IItem|undefined = new DivineAmulet();
+export const MageRingItem: IItem|undefined = new RingOfFire();
+export const ThiefDaggerItem: IItem|undefined = new Dagger();
 
 // TREASURE ASSIGNMENTS
-// todo: assign treasure from chests
 export function GetItemsInTreasureChests(): IItem[]{
   return [
-    new UselessAmulet(), //this will be found by the warrior
-    new UselessAmulet(), //this will be found by the cleric
-    new UselessAmulet(), //this will be found by the mage
-    new UselessAmulet(), //this will be found by the thief
+    new LargeHealthPotion(), //this will be found by the warrior
+    new SmallHealthPotion(), //this will be found by the cleric
+    new SmallHealthPotion(), //this will be found by the mage
+    new LargeHealthPotion(), //this will be found by the thief
   ];
 }
